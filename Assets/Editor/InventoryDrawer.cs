1b84f63c652b6f988034bbbe2eb2ca01e08bd2e9
Vinson Kok <vinsonkok@yahoo.com>
2020-10-07 22:12:00 -0500

    Inventory Property Drawer

---
+++ Assets/Editor/InventoryDrawer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+// InventoryDrawer
+[CustomPropertyDrawer(typeof(Equipment))]
+public class InventoryDrawer : PropertyDrawer
+{
+    // draw property in given rect
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        // draw label
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        // don't indent child fields
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        // calculate rects
+        var nameRect = new Rect(position.x - 55, position.y, 75, position.height);
+        var valueRect = new Rect(position.x + 30, position.y, 35, position.height);
+        var tierRect = new Rect(position.x + 75, position.y, 75, position.height);
+        var slotRect = new Rect(position.x + 160, position.y, 60, position.height);
+
+        // draw fields - pass GUIContent.none to each so they are drawn without labels
+        EditorGUI.PropertyField(nameRect, property.FindPropertyRelative("Name"), GUIContent.none);
+        EditorGUI.PropertyField(valueRect, property.FindPropertyRelative("Value"), GUIContent.none);
+        EditorGUI.PropertyField(tierRect, property.FindPropertyRelative("Tier"), GUIContent.none);
+        EditorGUI.PropertyField(slotRect, property.FindPropertyRelative("EquipmentSlot"), GUIContent.none);
+
+        // reset indent
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public enum Rarity { Common, Rare, Epic, Legendary }
+
+public enum Slot { Head, Chest, Arms, Legs }
+
+// Custom serializable class
+[Serializable]
+public class Equipment
+{
+    public string Name;
+    public int Value;
+    public Rarity Tier;
+    public Slot EquipmentSlot;
+}
+
+public class Inventory : MonoBehaviour
+{
+    public Equipment[] CurrentlyEquipped;
+}
